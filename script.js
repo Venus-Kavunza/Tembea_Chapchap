ad620a6bb3aa662ee00039d0e81eb56764cfcb55
Venus-Kavunza <venusmwende@gmail.com>
2022-01-19 23:19:14 +0300

    It will add validation to Contactus page

--- script.js
+++ script.js
@@ -37,3 +37,54 @@ function pass_Verify();{
         return true;    
     }
 }
+
+// Validation for Contact us
+
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelector("#error");
+
+function validateForm(){
+
+    clearMessage();
+    let errorFlag = false;
+
+    if(nameInput.value.length < 1){
+        errorNode[0].innerText = "Names cannot be blank";
+        nameInput.classList.add("error.border");
+        errorFlag =true;
+    }
+
+    if(!emailIsValid(email.value)){
+        errorNode[1].innerText = "Invalid email address";
+        email.classList.add("error.border");
+        errorFlag =true;
+    }
+
+    if(message.value.length < 1){
+        errorNode[2].innerText = "Please enter message";
+        message.classList.add("error.border");
+        errorFlag =true;
+    }
+
+    if(!errorFlag){
+        success.innerText = "Success!";
+    }
+}
+
+function clearMessages(){
+    for(let i = 0; i < errorNodes.length; i++){
+        errorNodes[i].innerText = "";
+    }
+    success.innerText ="";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+function emailIsValid(email){
+    let pattern = /\S+@\S+.\S+/;
+    return pattern.test(email);
+}
